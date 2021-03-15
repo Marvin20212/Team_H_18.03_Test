4e434c82f2ef7352df63981ac1196c1e2de15c06
Marvin20212 <marvin.mueller@student.jade-hs.de>
2021-03-15 15:09:00 +0100

    Update 1

---
+++ Team H Test/Team H Test/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+/*
+ Hello World Programm
+ Version 1.0
+ letzte Aenderung 15.03.2021
+ Team H HSP
+*/
+
+namespace Hello_World_Team_H
+{
+    class Program
+    {
+        static void Main(string[] args) // Hier beginnt das Hauptprogramm 
+        {
+            // Ausgabe Benutzer 1
+            Console.WriteLine("Hello World von XXX");
+
+            // Ausgabe Benutzer 2
+            Console.WriteLine("Nǐ hǎo, shìjiè");
+
+            // Ausgabe Benutzer 3
+            Console.WriteLine("Hallo Welt von XXX");
+
+            // Ausgabe Benutzer 4
+            Console.WriteLine("marhabaan bialealam");
+
+            // warte bis der User eine Taste drueckt
+            Console.ReadKey();
+
+        } // Hier endet das Hauptprogramm
+    }
+}
